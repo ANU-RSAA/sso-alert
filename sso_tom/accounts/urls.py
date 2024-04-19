d41bc4e8cc145192354f729b3fcf8464794c87a7
Shibli Saleheen <ssaleheen@swin.edu.au>
2024-04-19 13:35:52 +1000

    fix submit bug for create observation form and initial password reset

--- sso_tom/accounts/urls.py
+++ sso_tom/accounts/urls.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.contrib.auth import views as auth_views
 
 from .forms.password_change import PasswordChangeCustomForm
+from .forms.password_reset import PasswordResetCustomForm
 from .views import profile, registration
 
 
@@ -18,4 +19,26 @@ urlpatterns = [
          auth_views.PasswordChangeDoneView.as_view(
              template_name='accounts/registration/password_change_done.html',
          ), name='password_change_done'),
+
+    path('password_reset/',
+         auth_views.PasswordResetView.as_view(
+             from_email=settings.EMAIL_FROM,
+             template_name='accounts/registration/password_reset_form.html',
+             email_template_name='accounts/registration/password_reset_email.html',
+             subject_template_name='accounts/registration/password_reset_subject.txt',
+             html_email_template_name='accounts/registration/password_reset_email_html.html',
+         ), name='password_reset'),
+    path('password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='accounts/registration/password_reset_done.html',
+         ), name='password_reset_done', ),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             form_class=PasswordResetCustomForm,
+             template_name='accounts/registration/password_reset_confirm.html',
+         ), name='password_reset_confirm'),
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='accounts/registration/password_reset_complete.html',
+         ), name='password_reset_complete'),
 ]
---
+++ sso_tom/accounts/utility.py
@@ -0,0 +1,102 @@
+"""
+Distributed under the MIT License. See LICENSE.txt for more info.
+"""
+
+import logging
+
+from datetime import timedelta
+
+from django.utils import timezone
+from django.conf import settings
+
+from . import constants
+
+
+logger = logging.getLogger(__name__)
+
+
+def get_email_verification_expiry():
+    """
+    Finds the email verification expiry and then returns it.
+    :return: Number
+    """
+    try:
+        return settings.EMAIL_VERIFICATION_EXPIRY
+    except AttributeError:
+        return constants.EMAIL_VERIFICATION_EXPIRY
+
+
+def get_absolute_site_url(protocol=None, site_name=None):
+    """
+    Finds the site url that will be used to generate links
+    :param protocol: protocol defined in the request object
+    :param site_name: site_name defined in the request object
+    :return: String of the absolute url
+    """
+
+    # check whether forcefully using a specific url from the settings
+    if settings.SITE_URL != '':
+        return settings.SITE_URL
+
+    if not site_name:
+        return None
+
+    if protocol != 'https':
+        try:
+            # Look for protocol forcefully defined in the settings
+            protocol = settings.HTTP_PROTOCOL
+        except AttributeError:
+            protocol = 'http'
+    return protocol + '://' + site_name
+
+
+# def get_token(information, validity=None):
+#     """
+#     Stores the information in the database and generates a corresponding token
+#     :param information: information that needs to be stored and corresponding token to be generated
+#     :param validity: for how long the token will be valid (in seconds)
+#     :return: token to be encoded in the url
+#     """
+#     if validity:
+#         now = timezone.localtime(timezone.now())
+#         expiry = now + timedelta(seconds=validity)
+#     else:
+#         expiry = None
+#     try:
+#         verification = Verification.objects.create(information=information, expiry=expiry)
+#         return verification.id.__str__()
+#     except:
+#         logger.info("Failure generating Verification token with {}".format(information))
+#         raise
+#
+#
+# def get_information(token, reuse=False):
+#     """
+#     Retrieves the information from the database for a particular token
+#     :param token: encoded token from email
+#     :param reuse: boolean to determine whether this information will be used again
+#     :return: the actual information
+#     """
+#     now = timezone.localtime(timezone.now())
+#     try:
+#         verification = Verification.objects.get(id=token, expiry__gte=now)
+#         if verification.verified:
+#             raise ValueError('Already verified')
+#         else:
+#             if not reuse:
+#                 verification.verified = True
+#                 verification.save()
+#         return verification.information
+#     except Verification.DoesNotExist:
+#         raise ValueError('Invalid or expired verification code')
+#     except Exception as e:
+#         logger.exception(e)  # should notify admins via email
+#         raise
+#
+#
+# def get_admins():
+#     """
+#     Returns a list of admins from the User table
+#     :return: a list of Users
+#     """
+#     return User.objects.filter(user_role_user__role__name=Role.CORE_MEMBER)
