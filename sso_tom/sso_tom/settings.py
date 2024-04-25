68561fa18f55671a25222eda09386105bfdc2b99
Ray Seikel <rseikel@bigpond.com>
2024-04-25 13:05:28 +1000

    for deployment login

--- sso_tom/sso_tom/settings.py
+++ sso_tom/sso_tom/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = "21+fgm=fgx41*$+4o1svu*^k_dfl@^74)!zbq#fa^2_#@ct5hj"
 DEBUG = True
 
 # ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]', '10.0.0.24', 'ssoalert.adacs-dev.cloud.edu.au'] # for mobile tests
-ALLOWED_HOSTS = ['ssoalert.adacs-dev.cloud.edu.au']
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]', '10.0.0.24', 'ssoalert.adacs-dev.cloud.edu.au']
+CSRF_TRUSTED_ORIGINS = ['https://ssoalert.adacs-dev.cloud.edu.au']
 
 # Application definition
 
