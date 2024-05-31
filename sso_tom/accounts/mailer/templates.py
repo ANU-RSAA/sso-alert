6a0f17aeba5121487dc834f7f147390d281c2bc2
Shibli Saleheen <ssaleheen@swin.edu.au>
2024-05-31 11:02:52 +1000

    send out emails after observation is completed

---
+++ sso_tom/accounts/mailer/templates.py
@@ -0,0 +1,17 @@
+"""
+Distributed under the MIT License. See LICENSE.txt for more info.
+"""
+
+# Templates for different emails
+OBSERVATION_COMPLETED = dict()
+OBSERVATION_COMPLETED['subject'] = '[SSO Alerts] Notification - Observation Completed'
+OBSERVATION_COMPLETED['message'] = '<p>Dear {{first_name}} {{last_name}}: </p>' \
+                                   '<p>This is to let you know that your submitted observation has been completed. ' \
+                                   '<p>' \
+                                   'Facility: {{facility}}<br />' \
+                                   'Target: {{target}}<br />' \
+                                   'Status: {{observation_status}}' \
+                                   '</p>' \
+                                   '<p> </p>' \
+                                   '<p>Regards, </p>' \
+                                   '<p>SSO Alerts Team</p>'
