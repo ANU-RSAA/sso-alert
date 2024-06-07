1cbd5e16e0fe890f515c234e03b2b82eb50ae563
Shibli Saleheen <ssaleheen@swin.edu.au>
2024-06-07 11:34:38 +1000

    update the status update email

--- sso_tom/accounts/mailer/templates.py
+++ sso_tom/accounts/mailer/templates.py
@@ -4,9 +4,9 @@ Distributed under the MIT License. See LICENSE.txt for more info.
 
 # Templates for different emails
 OBSERVATION_COMPLETED = dict()
-OBSERVATION_COMPLETED['subject'] = '[SSO Alerts] Notification - Observation Completed'
+OBSERVATION_COMPLETED['subject'] = '[SSO Alerts] Notification - Observation status updated.'
 OBSERVATION_COMPLETED['message'] = '<p>Dear {{first_name}} {{last_name}}: </p>' \
-                                   '<p>This is to let you know that your submitted observation has been completed. ' \
+                                   '<p>This is to let you know that your submitted observation has been updated. ' \
                                    '<p>' \
                                    'Facility: {{facility}}<br />' \
                                    'Target: {{target}}<br />' \
