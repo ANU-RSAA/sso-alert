0ca50d239f7459e925287a311ea840817d876fe5
Shibli Saleheen <ssaleheen@swin.edu.au>
2024-11-11 12:42:21 +1100

    load secret key from .env

--- sso_tom/sso_tom/settings.py
+++ sso_tom/sso_tom/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "21+fgm=fgx41*$+4o1svu*^k_dfl@^74)!zbq#fa^2_#@ct5hj"
+SECRET_KEY = dotenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
