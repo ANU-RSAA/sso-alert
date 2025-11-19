17e77ab1bc477d4d114b5fab406f162ce5dc37f7
jamiesoon <jamiesoon@users.noreply.github.com>
2025-11-19 15:08:33 +1100

    remove unused imports and prings

--- sso_tom/chained/utils.py
+++ sso_tom/chained/utils.py
@@ -1,29 +1,26 @@
-import uuid
-
 import logging
 
-import requests
-from decouple import config
 from tom_observations.facility import get_service_class
 from tom_observations.models import ObservationRecord
 
-from .models import ChainedObservation, Chain
 from sso_tom.utils import submit_to_facility
+
+from .models import Chain, ChainedObservation
 
 logger = logging.getLogger(__name__)
 
-ADACS_PROPOSALDB_TEST_PASSWORD = config('ADACS_PROPOSALDB_TEST_PASSWORD')
-ADACS_PROPOSALDB_TEST_USERNAME = config('ADACS_PROPOSALDB_TEST_USERNAME')
-print(f"TOKENS FOR ACCESS {ADACS_PROPOSALDB_TEST_PASSWORD} {ADACS_PROPOSALDB_TEST_USERNAME}")
-
 
 def submit_chain(chain):
-    first_chained_observation = chain.chained_observations.filter(observation=None).order_by('created').first()
+    first_chained_observation = (
+        chain.chained_observations.filter(observation=None).order_by("created").first()
+    )
 
     if first_chained_observation:
         facility = get_service_class(first_chained_observation.facility)()
 
-        observation_ids, params = submit_to_facility(facility, first_chained_observation.parameters, chain.target)
+        observation_ids, params = submit_to_facility(
+            facility, first_chained_observation.parameters, chain.target
+        )
 
         # Create Observation record
         record = ObservationRecord.objects.create(
@@ -31,7 +28,7 @@ def submit_chain(chain):
             user=chain.user,
             facility=facility.name,
             parameters=params,
-            observation_id=observation_ids[0]
+            observation_id=observation_ids[0],
         )
 
         first_chained_observation.observation = record
@@ -48,11 +45,11 @@ def create_chain_and_submit_first(target, template_chained, user, topic=None):
     chain = Chain.objects.create(
         target=target,
         user=user,
-        name=f'{target.name}_FROM_STREAM',
-        description=f'{target.name}_FROM_STREAM. Retrieved as part of the topic = {topic}'
+        name=f"{target.name}_FROM_STREAM",
+        description=f"{target.name}_FROM_STREAM. Retrieved as part of the topic = {topic}",
     )
 
-    chained_templates = template_chained.chained_templates.all().order_by('created')
+    chained_templates = template_chained.chained_templates.all().order_by("created")
 
     for chained_template in chained_templates:
         ChainedObservation.objects.create(
